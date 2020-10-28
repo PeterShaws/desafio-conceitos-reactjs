c81ad4eb93f072f4dca4530ba6f3cfbec02c9d09
Pedro A. C. Jr <pchavesjr@gmail.com>
2020-10-27 21:19:19 -0300

    feature: Listar os repositórios da sua API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositores] = useState([]);
+
+  useEffect(() => {
+    async function fetchRepositories() {
+      try {
+        const response = await api.get("repositories");
+        setRepositores(response.data);
+      } catch (error) {
+        console.warn(error.response || error);
+      }
+    }
+    fetchRepositories();
+  }, []);
+
   async function handleAddRepository() {
     // TODO
+    console.log("handleAddRepository");
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    console.log("handleRemoveRepository", id);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
